643ad20afffecd249b2b0b007396ab1942a73a00
Robot1235369 <IsaacCA2018@gmail.com>
2022-03-22 17:23:49 +0000

    made window resizable

--- vars.py
+++ vars.py
@@ -2,7 +2,7 @@ import pygame
 
 WIDTH, HEIGHT = 500, 500
 TITLE = "Don't Run Out of Bullets"
-display = pygame.display.set_mode((WIDTH, HEIGHT), RESIZABLE)
+display = pygame.display.set_mode((WIDTH, HEIGHT), pygame.RESIZABLE)
 pygame.display.set_caption(TITLE)
 
 FPS = 60
