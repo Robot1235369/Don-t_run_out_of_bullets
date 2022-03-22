95918c8ffb592a15733a557e3b6a5ff56a42d163
Robot1235369 <IsaacCA2018@gmail.com>
2022-03-22 17:30:19 +0000

    stuff

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ import pygame
 from vars import *
 pygame.init()
 
+def fulscreen():
+    display.fill(BLACK)
+    pygame.display.update()
+
 def update():
     display.fill(BLACK)
     pygame.display.update()
@@ -13,7 +17,10 @@ def main():
         for event in pygame.events.get():
             if event == pygame.QUIT():
                 running = False
-        update()
+        if display.get_size() == (500 ,500):
+            fulscreen()
+        else:
+            update()
     pygame.quit()
 
 if __name__ == "__main__":
