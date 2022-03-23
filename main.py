2b43e943430c0296128c649645dfccf593fba4e4
Robot1235369 <IsaacCA2018@gmail.com>
2022-03-22 19:43:48 -0500

    working

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pygame
 from vars import *
 pygame.init()
 
-def fulscreen():
+def fullscreen():
     display.fill(BLACK)
     pygame.display.update()
 
@@ -11,14 +11,14 @@ def update():
     pygame.display.update()
 
 def main():
-    clock = pygame.time.CLOCK()
+    clock = pygame.time.Clock()
     while running:
         clock.tick(FPS)
         for event in pygame.events.get():
             if event == pygame.QUIT():
                 running = False
         if display.get_size() == (500 ,500):
-            fulscreen()
+            fullscreen()
         else:
             update()
     pygame.quit()
